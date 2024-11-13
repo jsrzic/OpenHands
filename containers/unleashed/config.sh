3d0bb524a13d1ccb7a9ee8197b078410417f36af
jsrzic <josip.srzic@gmail.com>
2024-11-13 01:14:38 +0100

    Add Dockerfile for unleashed runtime environment

---
+++ containers/unleashed/config.sh
@@ -0,0 +1,4 @@
+DOCKER_REGISTRY=docker.io
+DOCKER_ORG=jsrzic
+DOCKER_IMAGE=preconfigured-openhands
+DOCKER_BASE_DIR="."
---
+++ containers/unleashed/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eo pipefail
+
+echo "Starting OpenHands..."
+if [[ $NO_SETUP == "true" ]]; then
+  echo "Skipping setup, running as $(whoami)"
+  "$@"
+  exit 0
+fi
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "The OpenHands entrypoint.sh must run as root"
+  exit 1
+fi
+
+if [ -z "$WORKSPACE_MOUNT_PATH" ]; then
+  # This is set to /opt/workspace in the Dockerfile. But if the user isn't mounting, we want to unset it so that OpenHands doesn't mount at all
+  unset WORKSPACE_BASE
+fi
+
+/bin/bash -c "${*@Q}" # This magically runs any arguments passed to the script as a command
